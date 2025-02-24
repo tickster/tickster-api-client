25e161277c1fb1f5ab35234abad8f4beeddb6755
JohanThestrup <johan.thestrup@avidity.se>
2025-02-24 16:49:32 +0100

    Add method for getting upcoming events

---
+++ src/Tickster.Api/Models/Event/EventSummaryResourceCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace Tickster.Api.Models.Event;
+
+public class EventSummaryResourceCollection
+{
+    public int TotalItems { get; set; }
+    public int Skipped { get; set; }
+    public List<EventSummary> Items { get; set; } = [];
+    public List<ResourceLink> Links { get; set; } = [];
+}
